c72945fc581e01d96ea91341576429e3b11e0e87
Will Engellenner <will.engellenner@kellogg.northwestern.edu>
2023-04-28 18:58:22 +0000

    Finish 0

--- code-along/0-classes.rb
+++ code-along/0-classes.rb
@@ -4,5 +4,24 @@
 # rails runner code-along/0-classes.rb
 
 # String Class
+favorite_food = "tacos"
+bens_favorite_food = "kale"
 
 # String Methods
+puts favorite_food.class
+puts bens_favorite_food.class
+
+puts favorite_food.upcase
+puts bens_favorite_food.upcase
+
+class Dog < Hash
+    def speak
+        "woof"
+    end
+end
+
+my_dog = Dog.new
+my_dog["name"] = "Jenkins"
+puts "my name is #{my_dog["name"]}"
+
+puts my_dog.speak
